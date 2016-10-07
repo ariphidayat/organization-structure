f89f28693faf95e7090fe475de5eb414499ab8c7
Arip Hidayat <ariphidayat@hotmail.com>
2016-10-07 10:52:13 +0700

    version 2

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ buildTree(data);
 
 $('.tree li:has(ul)').addClass('parent').find(' > div').attr('title', 'Collapse');
 $('.tree li.parent > div').on('click', function (e) {
-    var children = $(this).parent('li.parent').find(' > ul > li');
+    var children = $(this).parent('li.parent').find(' > ul');
     if (children.is(":visible")) {
         children.hide('fast');
         $(this).attr('title', 'Expand');
