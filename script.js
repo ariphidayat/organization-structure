a9d84bf31fa48a2d0516aa7141513e32c1fdccd5
Arip Hidayat <ariphidayat@hotmail.com>
2016-10-08 22:48:36 +0700

    Indented by level
    please refer to this stack answer :
    http://stackoverflow.com/a/39919519/3846688

--- script.js
+++ script.js
@@ -1,59 +1,63 @@
-var data = [{
-    "id": 1,
-    "parent_id": 0,
-    "name": "BOD"
-}, {
-    "id": 2,
-    "parent_id": 1,
-    "name": "Finance Division"
-}, {
-    "id": 3,
-    "parent_id": 2,
-    "name": "Finance Department"
-}, {
-    "id": 4,
-    "parent_id": 2,
-    "name": "Accounting Department"
-}, {
-    "id": 5,
-    "parent_id": 1,
-    "name": "Human Resource Division"
-}, {
-    "id": 6,
-    "parent_id": 5,
-    "name": "Human Resource Department"
-}];
+var data = [
+    { "id": 1, "level": 1, "parent_id": 0, "name": "Board of Director" }, 
+    { "id": 2, "level": 2, "parent_id": 1, "name": "Finance Division" }, 
+    { "id": 3, "level": 3, "parent_id": 2, "name": "Finance Department" }, 
+    { "id": 4, "level": 3, "parent_id": 2, "name": "Sales Department" }, 
+    { "id": 5, "level": 3, "parent_id": 1, "name": "HR Division" }
+];
 
-var ul = $("#container");
+var tree = function(data, root) {
+    var r, o = {};
+    data.forEach(function(a) {
+        a.children = o[a.id] && o[a.id].children;
+        o[a.id] = a;
+        if (a.parent_id === root) {
+            r = a;
+        } else {
+            o[a.parent_id] = o[a.parent_id] || {};
+            o[a.parent_id].children = o[a.parent_id].children || [];
+            o[a.parent_id].children.push(a);
+        }
+    });
+    return r;
+}(data, 0);
 
-var buildTree = function(data) {
-  for (var i = 0; i < data.length; i++) {
-    var htmlStr = "<li id='" + data[i].id + "'>";
-    // var htmlStr = htmlStr + "<div>";
-    var htmlStr = htmlStr + "<div>" + data[i].name + "</div>";
-    // var htmlStr = htmlStr + "<div>";
-    var htmlStr = htmlStr + "</li>";
-    var parent;
+var ul = document.createElement('ul');
 
-    if (data[i].parent_id === 0) {
-      parent = ul;
-    } else {
-      var $elem = ul.find("li[id='" + data[i].parent_id + "']");
-      if ($elem.find("ul").length == 0) {
-          $elem.append("<ul>");
-      }
-      parent = $elem.find("ul:first");
-    }
+[tree].forEach(function iter(level) {
+    return function(a) {
+        var li = document.createElement('li'),
+            div = document.createElement('div'),
+            ul,
+            l = level;
 
-    parent.append(htmlStr);
-  }
-}
+        this.appendChild(li);
+        while (++l < a.level) {
+            div.className = 'just-line';
+            li.className = 'no-padding';
+            li.appendChild(div);
+            ul = document.createElement('ul');
+            li.appendChild(ul);
+            li = document.createElement('li');
+            ul.appendChild(li);
+            div = document.createElement('div');
+        }
+        div.appendChild(document.createTextNode(a.name));
+        li.appendChild(div);
 
-buildTree(data);
+        if (a.children) {
+            ul = document.createElement('ul');
+            li.appendChild(ul);
+            a.children.forEach(iter(a.level), ul);
+        }
+    };
+}(0), ul);
+
+document.getElementById('tree').appendChild(ul);
 
 $('.tree li:has(ul)').addClass('parent').find(' > div').attr('title', 'Collapse');
-$('.tree li.parent > div').on('click', function (e) {
-    var children = $(this).parent('li.parent').find(' > ul > li');
+$('.tree li.parent > div').on('click', function(e) {
+    var children = $(this).parent('li.parent').find(' > ul');
     if (children.is(":visible")) {
         children.hide('fast');
         $(this).attr('title', 'Expand');
